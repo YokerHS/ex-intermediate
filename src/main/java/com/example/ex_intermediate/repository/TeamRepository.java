6a315d3162ffeb81b3803b173071774857840a38
YokerHS <hiimyoker@gmail.com>
2025-05-26 10:22:46 +0900

    finAllTeam のjavadocの修正

--- src/main/java/com/example/ex_intermediate/repository/TeamRepository.java
+++ src/main/java/com/example/ex_intermediate/repository/TeamRepository.java
@@ -1,4 +1,53 @@
 package com.example.ex_intermediate.repository;
 
-public class TeamRepsitory {
+import com.example.ex_intermediate.domain.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TeamRepository {
+    /**
+     * Teamオブジェクトを生成するローマッパー.
+     */
+//    private static final RowMapper<Team> TEAM_ROW_MAPPER = (rs, i) -> {
+//        Team team = new Team();
+//        team.setId(rs.getInt("id"));
+//        return team;
+//    };
+    private static final RowMapper<Team> TEAM_ROW_MAPPER = new BeanPropertyRowMapper<>(Team.class);
+
+    private final NamedParameterJdbcTemplate template;
+
+    public TeamRepository(NamedParameterJdbcTemplate template){
+        this.template = template;
+    }
+
+    /**
+     * チーム情報をすべて取得します.
+     *
+     * @return チームの一覧リスト
+     */
+    public List<Team> findAllTeams() {
+        String sql = """
+        SELECT
+            id,
+            league_name,
+            team_name,
+            headquarters,
+            inauguration,
+            history
+        FROM
+            teams
+    """;
+
+        return template.query(sql, TEAM_ROW_MAPPER);
+    }
+
+
 }
