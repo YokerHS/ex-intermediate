22f7eacda24b99327524203dd8afb0fef74a5fe2
YokerHS <hiimyoker@gmail.com>
2025-05-26 10:31:11 +0900

    showAllTeams メソッドの追加

--- src/main/java/com/example/ex_intermediate/service/TeamService.java
+++ src/main/java/com/example/ex_intermediate/service/TeamService.java
@@ -1,4 +1,22 @@
 package com.example.ex_intermediate.service;
 
+import com.example.ex_intermediate.domain.Team;
+import com.example.ex_intermediate.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class TeamService {
+    @Autowired
+    TeamRepository teamRepository;
+
+    /**
+     * すべてのチーム情報を取得して返します。
+     *
+     * @return チームの一覧リスト
+     */
+    public List<Team> showAllTeams() {
+        return teamRepository.findAllTeams();
+    }
+
 }
